8d01b2227a3d38b65107d3f2e79692f7424f5c67
nssmhmcgd <spadeliz@outlook.com>
2025-11-17 15:18:46 +0900

    updated

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -21,13 +21,20 @@ void initialize_list(list* l)
 // 先頭にデータを追加
 void push_front(list* l, node* p)
 {
-	// ToDo: ここにコードを追加
+	if (!p) return;
+	
+	p->pNext = l->header;
+	l->header = p;
 }
 
 // pの次のノードを削除
 void remove_next(list* l, node* p)
 {
-	// ToDo: ここにコードを追加
+	if (!p) return;
+	if (p->pNext == NULL) return;
+	
+	node* temp = p->pNext;
+	p->pNext = temp->pNext;
 }
 
 
